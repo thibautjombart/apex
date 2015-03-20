045bbc177c1fbe715a426108f4d7395e7373a9a9
RBHarris <rbharris@uw.edu>
2015-03-20 09:58:09 -0400

    Fasta files and NULL roxygen doc.

---
+++ inst/chickadee_fasta.R
@@ -0,0 +1,7 @@
+#' @title FASTA
+#' @description Anonymous nuclear loci from Black-capped chickadees (Poecile atricapillus).
+#' @description Data used in Harris et al. 2013.
+#' 
+#' @author Rebecca Harris <rbharris@@uw.edu>
+
+NULL
