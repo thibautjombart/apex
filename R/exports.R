12a544ec04c584fe7f3ebbd9f6a1ad712c5d0e4b
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-03-20 19:44:05 -0400

    add mlst and gapIsNA options to multidna2genind.

--- R/exports.R
+++ R/exports.R
@@ -5,8 +5,10 @@
 #'
 #' @param x a \linkS4class{multidna} object.
 #' @param genes an optional vector indicating the genes to retain for the concatenation; any way to subset the list in x@@dna is acceptable; by default, all genes are used.
+#' @param mlst if \code{TRUE}, each gene will result in a single locus in the genind object. (Default to \code{FALSE})
+#' @param gapIsNA if \code{TRUE} and \code{mlst = TRUE}, sequences that consist entirely of gaps will be considered as NAs. (Default to \code{FALSE})
 #'
-#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
+#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}, Zhian N. Kamvar
 #'
 #' @aliases multidna2genind
 #'
@@ -31,19 +33,30 @@
 #' x
 #'
 #'
-multidna2genind <- function(x, genes=TRUE, mlst=FALSE){
+multidna2genind <- function(x, genes=TRUE, mlst=FALSE, gapIsNA=FALSE){
   if (!mlst){
     return(DNAbin2genind(concatenate(x, genes=genes)))
   } 
   xlist  <- lapply(x@dna, function(i) apply(as.character(i), 1, paste, collapse = ""))
   xdf    <- data.frame(xlist)
-  xdfnum <- data.frame(lapply(xdf, as.numeric))
   xlevs  <- lapply(xdf, levels)
+  if (gapIsNA){
+    xgap <- find_gap_sequence(xlevs)
+    for (i in names(xgap)){
+      levels(xdf[[i]])[xgap[[i]]] <- NA
+    }    
+  }
+  xdfnum <- data.frame(lapply(xdf, as.numeric))
   xgid   <- df2genind(xdfnum, ploidy = 1, ind.names = x@labels)
-  names(xlevs) <- names(xgid@all.names)
+  names(xlevs)   <- names(xgid@all.names)
   xgid@all.names <- xlevs
   xgid@other$ind.info <- x@ind.info
   xgid@other$gene.info <- x@gene.info
   return(xgid)
+}
+
+find_gap_sequence <- function(x){
+  wheregaps <- lapply(x, function(i) which(grepl("^\\-+?$", i)))
+  return(wheregaps)
 }
 
