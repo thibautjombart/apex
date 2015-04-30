84f984b9428c50dcafe688a8ee3bfccb73e26e00
Thibaut Jombart <thibautjombart@gmail.com>
2015-04-30 15:35:32 +0100

    adding function add.gaps

---
+++ R/add.gaps.R
@@ -0,0 +1,55 @@
+#'
+#' Add gap-only sequences for missing data
+#'
+#' In \linkS4class{multidna} and \linkS4class{multiphyDat}, some individuals may not be sequenced for all genes.
+#' The generic function \code{add.gap} has method for both objects; it identifies the missing sequences, and adds gap-only sequences to the alignments wherever needed.
+#'
+#'
+#' @docType methods
+#'
+#' @export
+#'
+#' @aliases add.gaps
+#'
+#' @param x a \linkS4class{multidna} or \linkS4class{multiphyDat} object.
+#' @param ... further arguments passed to other methods (currently not used).
+#'
+#' @aliases add.gaps.generic
+#' @aliases add.gaps.multidna
+#' @aliases add.gaps.multiphyDat
+#' @aliases add.gaps,multidna-method
+#' @aliases add.gaps,multiphyDat-method
+#'
+setGeneric("add.gaps", function(x, ...) standardGeneric("add.gaps"))
+
+
+
+#' @rdname add.gap
+#'
+#' @export
+#'
+setMethod("add.gaps", "multidna", function(x, ...){
+    ## ESCAPE IF NO DNA SEQUENCES ##
+    if(is.null(x@dna)) return(x)
+
+    ## FUNCTION TO REORDER MATRIX AND ADD MISSING SEQUENCES ##
+    form.dna.matrix <- function(mat.dna, labels){
+        ## make matrix of missing sequences if needed
+        lab.missing <- labels[!(labels %in% rownames(mat.dna))]
+        n.missing <- length(lab.missing)
+        if(n.missing>0){
+            mat.NA <- as.DNAbin(matrix("-", ncol=ncol(mat.dna), nrow=n.missing))
+            rownames(mat.NA) <- lab.missing
+            mat.dna <- rbind(mat.dna, mat.NA)
+        }
+
+        ## return ordered sequences
+        return(mat.dna[labels,])
+    }
+
+    ## APPLY THIS FUNCTION TO ALL MATRICES ##
+    x@dna <- lapply(x@dna, form.dna.matrix, x@labels)
+
+    ## RETURN OBJECT ##
+    return(x)
+}) # end multidna method
