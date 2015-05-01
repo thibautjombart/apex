32de4811acff7d01a33866452c47179dfa9b412e
Thibaut Jombart <thibautjombart@gmail.com>
2015-05-01 13:52:40 +0100

    making function to find gap only sequences working for DNAbin and phyDat

--- R/internal.R
+++ R/internal.R
@@ -5,10 +5,11 @@
 ## Thibaut Jombart, April 2015
 ##
 
-## compute the number of missing sequences for each gene in a list of DNAbin matrices 'x'
+## compute the number of missing sequences for each gene in a list of DNAbin matrices or phyDat objects 'x'
 .nMissingSequences <- function(x){
     ## only keep non-empty matrices
+    x <- lapply(x, as.character)
     x <- x[sapply(x, nrow)>0]
-    out <- sapply(x, function(e) sum(apply(e==as.DNAbin("-"),1,all)))
+    out <- sapply(x, function(e) sum(apply(e=="-",1,all)))
     return(sum(out,na.rm=TRUE))
 }
